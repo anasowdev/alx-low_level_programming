ec7a93ea4a012045a3755cd0bd7f539bb6d711eb
Anas LAMINE <anas.lamine@gmail.com>
2023-03-27 14:59:23 -0700

    fix for task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,41 +1,25 @@
 #include "main.h"
 
 /**
- *_atoi - convert a string to an integer.
- *@s: pointer to string
- *
- *Return: integer value of string, or 0 if no digits are found
+ * rev_string - Reverses a string
+ * @s: Input string
+ * Return: String in reverse
  */
-int _atoi(char *s)
+
+void rev_string(char *s)
 {
-	int sign = 1;
-	int result = 0;
-	int digit_found = 0;
+	char rev = s[0];
+	int counter = 0;
+	int i;
 
-	while (*s)
+	while (s[counter] != '\0')
+	counter++;
+	for (i = 0; i < counter; i++)
 	{
-		if (*s == '-')
-			sign *= -1;
-
-		if (*s >= '0' && *s <= '9')
-		{
-			digit_found = 1;
-			result = result *10 + (*s - '0');
-
-			/*Check for overflow */
-			if (result < 0)
-			{
-				if (sign == 1)
-					return 2147483647;
-				else
-					return -2147483648;
-			}
-		}
-		else if (digit_found)
-			break;
-
-		s++;
+		counter--;
+		rev = s[i];
+		s[i] = s[counter];
+		s[counter] = rev;
 	}
-
-	return (sign *result);
 }
+
